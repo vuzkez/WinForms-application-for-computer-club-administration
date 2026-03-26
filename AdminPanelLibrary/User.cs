2574b91797670fbb7254951f9aea084865e06a26
hetto <vuzikez89@gmail.com>
2026-03-26 20:36:19 +0300

    Update User.cs
    change string to int field UserId

--- AdminPanelLibrary/User.cs
+++ AdminPanelLibrary/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
