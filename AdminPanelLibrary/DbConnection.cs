639501dd657b731fe6339fa7d124d4d78e09923a
Ярослав Кислов <yaraslav15035@gmail.com>
2026-03-26 23:46:35 +0300

    add db context

---
+++ AdminPanelLibrary/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanelLibrary
+{
+    public static class DbConnection
+    {
+        public static string connectionString = @"Data Source=VUZKEZ\SQLEXPRESS;Initial Catalog=GameClub;Integrated Security=True";
+
+        public static DataContext GetConnection()
+        {
+            return new DataContext(connectionString);
+        }
+    }
+}
