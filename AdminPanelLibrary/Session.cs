c21734c68d7646034df5a98ccfe3a20323ab7e0a
Ярослав Кислов <yaraslav15035@gmail.com>
2026-03-26 20:43:07 +0300

    add object session

---
+++ AdminPanelLibrary/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanelLibrary
+{
+    public class Session
+    {
+        public int SessionId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
