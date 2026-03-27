e3c029b2823ce887a2308cd2ed28bf17bb3e7a50
Ярослав Кислов <yaraslav15035@gmail.com>
2026-03-27 18:55:24 +0300

    add operator and change DataContext

---
+++ AdminPanelLibrary/Operator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminPanelLibrary
+{
+    public class Operator : IOperator
+    {
+        private readonly IDataContext dataContext;
+
+        public Operator(IDataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+        public Seat FindFreeSeat(string roomType)
+        {
+            using (var db = dataContext.Create())
+            {
+
+            }
+        }
+
+        public void OpenSession(int seatId, int userId, TariffType tariff)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
